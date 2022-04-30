239e589054f2ebb5fb852f886dee7a58c8ad1d37
Matimed <mcarazomedley@gmail.com>
2022-04-30 13:14:40 +0000

    Add rect mode in tile_map_editor

--- tile_map_editor.py
+++ tile_map_editor.py
@@ -31,24 +31,58 @@ class TileMapEditor(MapEditor):
 
 
     def main(self):
-        while True:
+        while 1: 
+            try:
+                mode = self.ask_question('Select an editing mode (1: Lines | 2: Rects)')
+                if mode != '1' and mode != '2': AssertionError("invalid option")
+                else: break
+            except Exception as error:
+                print(f'Unexpected {error}. Try again \n')
+                continue
+
+        os.system('cls||clear')
+        while 1:
             try:
                 print("Type 'exit' to save and close the program \n")
                 print(self.tile_map , ' \n')
-                axis = self.ask_question('Which axis of the map you want to modify? (x | y)')
-                if axis != 'x' and axis != 'y': raise AssertionError("invalid option")
-                line = int(self.ask_question('Wich row/column do you want to modify? (starting at 0)'))
-                start = int(self.ask_question('From which position do you want to modify? (inclusive)'))
-                end = int(self.ask_question('Until which position do you want to modify? (inclusive)'))
-                print(f'Select the tile type to replace it with:')
-                tile = Tile(int(self.ask_question(self.list_to_str(self.get_list_of_elements(Tile)))))
-                self.modify_tile_map(axis, line, start, end, tile)
-                os.system('cls||clear')
+                if mode == '1': self.edit_by_lines()
+                elif mode == '2': self.edit_by_rectangules()
+                else: AssertionError("invalid option")
 
             except Exception as error:
                 print(f"Unexpected {error}. Try again \n")
                 continue
 
+
+    def edit_by_lines(self):
+        axis = self.ask_question('Which axis of the map you want to modify? (x | y)')
+        if axis != 'x' and axis != 'y': raise AssertionError("invalid option")
+        line = int(self.ask_question('Wich row/column do you want to modify? (starting at 0)'))
+        start = int(self.ask_question('From which position do you want to modify? (inclusive)'))
+        end = int(self.ask_question('Until which position do you want to modify? (inclusive)'))
+        print(f'Select the tile type to replace it with:')
+        tile = Tile(int(self.ask_question(self.list_to_str(self.get_list_of_elements(Tile)))))
+        if axis == 'x':
+            for pos in range(start, end + 1): self.tile_map.set_element((line, pos), tile)
+
+        if axis == 'y':
+            for pos in range(start, end + 1): self.tile_map.set_element((pos, line), tile)
+
+    def edit_by_rectangules(self):
+        row = int(self.ask_question('In which row is the first position? (starting at 0)'))
+        column = int(self.ask_question('In which column is the first position? (starting at 0)'))
+        start_point = (row,column)
+        self.tile_map.get_element(start_point)  # Validate the position.
+        row = int(self.ask_question('In which row is the last position? (starting at 0)'))
+        column = int(self.ask_question('In which column is the last position? (starting at 0)'))
+        end_point = (row,column)
+        self.tile_map.get_element(end_point)  # Validate the position.
+        print(f'Select the tile type to replace it with:')
+        tile = Tile(int(self.ask_question(self.list_to_str(self.get_list_of_elements(Tile)))))
+        
+        for row in range(start_point[0], end_point[0]+1):
+            for column in range(start_point[1], end_point[1]+1):
+                self.tile_map.set_element((row, column), tile)
     
     def create_new_map(self):
         rows = int(self.ask_question('Define the quantity of rows of your map:'))
@@ -56,15 +90,6 @@ class TileMapEditor(MapEditor):
         self.tile_map = Matrix([[Tile.EMPTY for x in range(column)] for i in range(rows)])
         os.system('cls||clear')
     
-    
-    def modify_tile_map(self, axis, line, start, end, tile):
-        if axis == 'x':
-            for pos in range(start, end + 1): self.tile_map.set_element((line, pos), tile)
-
-        if axis == 'y':
-            for pos in range(start, end + 1): self.tile_map.set_element((pos, line), tile)
-
-
     def save(self):
         if self.tile_map:
             name = self.ask_question('How do you want your map to be called?')
